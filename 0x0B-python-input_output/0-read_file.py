14d45fd7e8f648c67cb9d1a929d626aea49dc3a6
Charlesomondi-dot <charlesomondi232@gmail.com>
2024-01-10 02:05:28 +0300

    python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,7 +5,6 @@ Module 0-read_file
 Contains function that reads and prints contents from file
 """
 
-
 def read_file(filename=""):
     """Read and print text from file"""
         with open(filename, mode="r", encoding="utf-8") as f:
